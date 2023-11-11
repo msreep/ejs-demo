c77f746415017eff233a268d6c5517a91b3808a2
msreep <42392168+msreep@users.noreply.github.com>
2023-11-11 18:56:49 -0500

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -6,6 +6,6 @@ describe("/ endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/")
         expect(response.cookie.myname.toBe("Mike")
-        expect(response.status).toBe(200)
+        expect(response.status).toBe(200);
     })
 })
