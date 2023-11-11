6ff072a2766f2d0244ac051ecd503ae593075bd0
msreep <42392168+msreep@users.noreply.github.com>
2023-11-11 18:53:34 -0500

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -2,9 +2,10 @@ const app = require("./server-2")
 const supertest = require("supertest")
 const request = supertest(app)
 
-describe("/about endpoint", () => {
+describe("/ endpoint", () => {
     it("should return a response", async () => {
-        const response = await request.get("/about")
+        const response = await request.get("/")
+        expect(response.cookie.myname.toBe("Mike");
         expect(response.status).toBe(200)
     })
 })
