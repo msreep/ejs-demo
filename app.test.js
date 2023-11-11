4f57f79a1d03ab26023c2901f70134883516d83e
msreep <42392168+msreep@users.noreply.github.com>
2023-11-11 18:34:59 -0500

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -2,9 +2,9 @@ const app = require("./server-4")
 const supertest = require("supertest")
 const request = supertest(app)
 
-describe("/test endpoint", () => {
+describe("/about endpoint", () => {
     it("should return a response", async () => {
-        const response = await request.get("/test")
+        const response = await request.get("/about")
         expect(response.status).toBe(200)
     })
 })
